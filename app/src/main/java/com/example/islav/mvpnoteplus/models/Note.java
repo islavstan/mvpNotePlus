b3c39b2987b6aca471e20cf1a7b99f5d6ef75e9c
islavstan <islavstan@gmail.com>
2017-01-13 22:37:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/islav/mvpnoteplus/models/Note.java
@@ -0,0 +1,60 @@
+package com.example.islav.mvpnoteplus.models;
+
+import android.content.ContentValues;
+
+import com.example.islav.mvpnoteplus.data.DBSchema;
+
+
+
+public class Note {
+
+    private int id = -1;
+    private String mText;
+    private String mDate;
+
+    public Note() {
+    }
+
+    public Note(int id, String mText, String mDate) {
+        this.id = id;
+        this.mText = mText;
+        this.mDate = mDate;
+    }
+
+    public Note(String mText, String mDate) {
+        this.mText = mText;
+        this.mDate = mDate;
+    }
+
+    public ContentValues getValues(){
+        ContentValues cv = new ContentValues();
+        if ( id!=-1) cv.put(DBSchema.TB_NOTES.ID, id);
+        cv.put(DBSchema.TB_NOTES.NOTE, mText);
+        cv.put(DBSchema.TB_NOTES.DATE, mDate);
+        return cv;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(String mDate) {
+        this.mDate = mDate;
+    }
+
+    public void setText(String mText) {
+        this.mText = mText;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public String getText() {
+        return mText;
+    }
+}
